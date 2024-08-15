9c1cadb45800a67f780c44b98b66383d127c4c44
Kron <136763305+IKronito@users.noreply.github.com>
2024-08-15 20:04:19 -0300

    dadased

--- src/main/java/com/prisao/controllers/AgenteController.java
+++ src/main/java/com/prisao/controllers/AgenteController.java
@@ -1,5 +1,24 @@
 package com.prisao.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.prisao.entities.AgenteEntity;
+import com.prisao.services.AgenteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agente")
 public class AgenteController {
+
+    @Autowired
+    private AgenteService agenteService;
+
+    @PostMapping("/saveagente")
+    public AgenteEntity saveAgente(@RequestBody AgenteEntity agenteEntity){
+        return agenteService.saveAgente(agenteEntity);
+    }
 
 }
--- src/main/java/com/prisao/services/AgenteService.java
+++ src/main/java/com/prisao/services/AgenteService.java
@@ -1,5 +1,47 @@
 package com.prisao.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.prisao.entities.AgenteEntity;
+import com.prisao.repositories.AgenteRepository;
+
+@Service
 public class AgenteService {
+
+	@Autowired
+	private AgenteRepository agenteRepository;
+
+	//Encontrar por ID
+	public AgenteEntity findById(Long Id) {
+
+		try {
+			return agenteRepository.findById(Id).orElseThrow();
+		} catch (Exception e) {
+			return new AgenteEntity();
+		}
+
+	}
+
+	//Encontrar Todos os Agentes
+	public List<AgenteEntity> findAll() {
+		return agenteRepository.findAll();
+	}
+
+	//SalvarAgente
+	public AgenteEntity saveAgente(AgenteEntity agenteEntity) {
+
+		return agenteRepository.save(agenteEntity);
+	}
+
+	//Deletar Agente Por ID
+	public void deleteAgente(Long id) {
+		AgenteEntity agente = findById(id);
+		agenteRepository.delete(agente);
+	}
+	
+	
 
 }
